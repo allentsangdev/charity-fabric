07064bb170a0b8ddd06c54525c06f6241d06bd0b
allentsangdev <allentsang04@gmail.com>
2023-06-21 18:51:51 -0400

    enrollUser updates

--- client/app/api/enroll-user/route.js
+++ client/app/api/enroll-user/route.js
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server'
-const { main } = require('.././charity-gateway/enrollUser.js');
+import { main } from '/home/azureuser/fabric-samples/charity-fabric/charity-gateway/enrollUser.js'
 
-export async function GET() {
+export async function GET(Request) {
   const res = await main()
   
   const data = await res.json()
+  console.log(NextResponse.json({ data }));
  
   return NextResponse.json({ data })
+
+  // return NextResponse.json("Hi 2")
 }
